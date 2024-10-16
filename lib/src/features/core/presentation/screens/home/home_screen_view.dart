1e91779876cc1b94d49458aa4bf585fe9e165b9c
Karlo M <karlo.marinovic@decode.agency>
2024-10-16 17:56:21 +0200

    create modal for add daily busdget

--- lib/src/features/core/presentation/screens/home/home_screen_view.dart
+++ lib/src/features/core/presentation/screens/home/home_screen_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:svorc_proto_v1/src/features/core/presentation/widgets/home/add_monthly_budget.dart';
 import 'package:svorc_proto_v1/src/features/reports/application/bloc/cubits/home_screen_balances_report/home_screen_balances_report_cubit.dart';
 
 class HomeScreenView extends StatelessWidget {
@@ -23,61 +24,14 @@ class HomeScreenView extends StatelessWidget {
             is HomeScreenBalancesReportCubitStateCurrentMonthDailyBudgetNotFound) {
           showDialog(
             context: context,
+            barrierDismissible: false,
             builder: (context) {
-              return const _Some();
+              // TODO future work, prevent closing this in any way by user
+              return const AddMonthlyBudget();
             },
           );
         }
       },
-    );
-  }
-}
-
-class _Some extends StatelessWidget {
-  const _Some({
-    super.key,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return Dialog(
-      insetPadding: const EdgeInsets.all(15),
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(4),
-      ),
-      clipBehavior: Clip.antiAlias,
-      child: Container(
-        color: Colors.white,
-        padding: EdgeInsets.only(
-          bottom: MediaQuery.of(context).viewInsets.bottom,
-          top: 15,
-          left: 15,
-          right: 15,
-        ),
-        child: const Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text(
-                  "ADD DAILY BUDGET",
-                  style: TextStyle(
-                    fontSize: 16,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-                SizedBox(
-                  width: 10,
-                ),
-              ],
-            ),
-            SizedBox(
-              height: 30,
-            ),
-          ],
-        ),
-      ),
     );
   }
 }
