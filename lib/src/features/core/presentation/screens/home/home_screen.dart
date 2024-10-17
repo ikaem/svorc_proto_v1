1711ae41e121554c97d6f83904eb979bc8dc6470
Karlo M <karlo.marinovic@decode.agency>
2024-10-17 18:06:13 +0200

    use get daily budget cubit

--- lib/src/features/core/presentation/screens/home/home_screen.dart
+++ lib/src/features/core/presentation/screens/home/home_screen.dart
@@ -4,6 +4,7 @@ import 'package:flutter/widgets.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:svorc_proto_v1/src/features/core/presentation/screens/home/home_screen_view.dart';
 import 'package:svorc_proto_v1/src/features/expenses/domain/repositories/expenses_repository.dart';
+import 'package:svorc_proto_v1/src/features/period_daily_budgets/application/bloc/cubits/get_month_daily_budget/get_month_daily_budget_cubit.dart';
 import 'package:svorc_proto_v1/src/features/period_daily_budgets/data/entities/local/period_daily_budget/period_daily_budget_local_entity.dart';
 import 'package:svorc_proto_v1/src/features/period_daily_budgets/domain/models/period_daily_budget_model.dart';
 import 'package:svorc_proto_v1/src/features/period_daily_budgets/domain/repositories/period_daily_budgets_repository.dart';
@@ -16,35 +17,53 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return BlocProvider<GetCurrentMonthBalancesCubit>(
+    return BlocProvider<GetMonthDailyBudgetCubit>(
       create: (context) {
-        final ExpensesRepository expensesRepository =
-            context.read<ExpensesRepository>();
-        // final PeriodDailyBudgetsRepository budgetsRepository =
-        //     context.read<PeriodDailyBudgetsRepository>();
+        final PeriodDailyBudgetsRepository budgetsRepository =
+            context.read<PeriodDailyBudgetsRepository>();
+        final GetMonthDailyBudgetUseCase monthDailyBudgetUseCase =
+            GetMonthDailyBudgetUseCase(budgetsRepository);
 
-        final GetMonthBalancesUseCase balancesUseCase =
-            GetMonthBalancesUseCase(expensesRepository);
-        // final GetMonthDailyBudgetUseCase monthDailyBudgetUseCase =
-        //     GetMonthDailyBudgetUseCase(budgetsRepository);
+        final GetMonthDailyBudgetCubit monthDailyBudgetCubit =
+            GetMonthDailyBudgetCubit(
+          getMonthDailyBudgetUseCase: monthDailyBudgetUseCase,
+        );
 
-        final GetCurrentMonthBalancesCubit balancesReportCubit =
-            GetCurrentMonthBalancesCubit(
-          getHomeScreenBalancesUseCase: balancesUseCase,
-          // getMonthDailyBudgetUseCase: monthDailyBudgetUseCase,
-        )..onLoadBalances(
-                // TODO temp only
-                currentMonthDailyBudget: PeriodDailyBudgetModel(
-                  id: 1,
-                  periodStart: DateTime.now(),
-                  periodEnd: DateTime.now(),
-                  amount: 1000,
-                  period: Period.month,
-                ),
-              );
-        return balancesReportCubit;
+        return monthDailyBudgetCubit..onLoadBudget(date: DateTime.now());
       },
       child: const HomeScreenView(),
     );
+
+    // TODO old
+    // return BlocProvider<GetCurrentMonthBalancesCubit>(
+    //   create: (context) {
+    //     final ExpensesRepository expensesRepository =
+    //         context.read<ExpensesRepository>();
+    //     // final PeriodDailyBudgetsRepository budgetsRepository =
+    //     //     context.read<PeriodDailyBudgetsRepository>();
+
+    //     final GetMonthBalancesUseCase balancesUseCase =
+    //         GetMonthBalancesUseCase(expensesRepository);
+    //     // final GetMonthDailyBudgetUseCase monthDailyBudgetUseCase =
+    //     //     GetMonthDailyBudgetUseCase(budgetsRepository);
+
+    //     final GetCurrentMonthBalancesCubit balancesReportCubit =
+    //         GetCurrentMonthBalancesCubit(
+    //       getHomeScreenBalancesUseCase: balancesUseCase,
+    //       // getMonthDailyBudgetUseCase: monthDailyBudgetUseCase,
+    //     )..onLoadBalances(
+    //             // TODO temp only
+    //             currentMonthDailyBudget: PeriodDailyBudgetModel(
+    //               id: 1,
+    //               periodStart: DateTime.now(),
+    //               periodEnd: DateTime.now(),
+    //               amount: 1000,
+    //               period: Period.month,
+    //             ),
+    //           );
+    //     return balancesReportCubit;
+    //   },
+    //   child: const HomeScreenView(),
+    // );
   }
 }
--- lib/src/features/core/presentation/screens/home/home_screen_view.dart
+++ lib/src/features/core/presentation/screens/home/home_screen_view.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:svorc_proto_v1/src/features/core/presentation/widgets/home/add_monthly_budget_dialog.dart';
+import 'package:svorc_proto_v1/src/features/period_daily_budgets/application/bloc/cubits/get_month_daily_budget/get_month_daily_budget_cubit.dart';
 import 'package:svorc_proto_v1/src/features/reports/application/bloc/cubits/get_current_month_balances/get_current_month_balances_cubit.dart';
 
 class HomeScreenView extends StatelessWidget {
@@ -8,37 +9,64 @@ class HomeScreenView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return BlocConsumer<GetCurrentMonthBalancesCubit,
-        GetCurrentMonthBalancesCubitState>(
+    return BlocConsumer<GetMonthDailyBudgetCubit,
+        GetMonthDailyBudgetCubitState>(
       builder: (context, state) {
-        return Scaffold(
-          body: SafeArea(
-            child: _HomeScreenViewBody(
-              state: state,
+        return switch (state) {
+          // TODO: Handle this case.
+          (GetMonthDailyBudgetCubitStateInitial _) =>
+            const Center(child: Text("Initial stuff")),
+          (GetMonthDailyBudgetCubitStateLoading _) => const Center(
+              child: CircularProgressIndicator(),
             ),
-          ),
-        );
+          (GetMonthDailyBudgetCubitStateSuccess _) => const Center(
+              child: Text("Success"),
+            ),
+          (GetMonthDailyBudgetCubitStateFailure _) => const Center(
+              child: Text("Failure"),
+            ),
+          // TODO: Handle this case.
+          (GetMonthDailyBudgetCubitStateMonthDailyBudgetNotFound _) =>
+            const Center(
+              child: Text("Not Found"),
+            ),
+        };
       },
-      // TODO not needed
       listener: (context, state) {},
-      // listener: (context, state) {
-      //   if (state
-      //       is HomeScreenBalancesReportCubitStateCurrentMonthDailyBudgetNotFound) {
-      //     showDialog(
-      //       context: context,
-      //       barrierDismissible: false,
-      //       builder: (context) {
-      //         // TODO future work, prevent closing this in any way by user
-      //         return AddMonthlyBudgetDialog(
-      //           onClose: () {
-      //             Navigator.of(context).pop();
-      //           },
-      //         );
-      //       },
-      //     );
-      //   }
-      // },
     );
+
+    // TODO old
+    // return BlocConsumer<GetCurrentMonthBalancesCubit,
+    //     GetCurrentMonthBalancesCubitState>(
+    //   builder: (context, state) {
+    //     return Scaffold(
+    //       body: SafeArea(
+    //         child: _HomeScreenViewBody(
+    //           state: state,
+    //         ),
+    //       ),
+    //     );
+    //   },
+    //   // TODO not needed
+    //   listener: (context, state) {},
+    //   // listener: (context, state) {
+    //   //   if (state
+    //   //       is HomeScreenBalancesReportCubitStateCurrentMonthDailyBudgetNotFound) {
+    //   //     showDialog(
+    //   //       context: context,
+    //   //       barrierDismissible: false,
+    //   //       builder: (context) {
+    //   //         // TODO future work, prevent closing this in any way by user
+    //   //         return AddMonthlyBudgetDialog(
+    //   //           onClose: () {
+    //   //             Navigator.of(context).pop();
+    //   //           },
+    //   //         );
+    //   //       },
+    //   //     );
+    //   //   }
+    //   // },
+    // );
   }
 }
 
