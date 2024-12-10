5bf4924e52e39565cd440f0e6c0618028b719c0b
Karlo M <karlo.marinovic@decode.agency>
2024-12-10 19:39:52 +0100

    extract home screen top button widgets

--- lib/src/features/core/presentation/widgets/home/home_screen_top_buttons.dart
+++ lib/src/features/core/presentation/widgets/home/home_screen_top_buttons.dart
@@ -28,239 +28,277 @@ class HomeScreenTopButtons extends StatelessWidget {
         children: [
           // TODO all of these should be extracted in their own widgets
           Flexible(
-            child: Container(
-              decoration: BoxDecoration(
-                color: Colors.grey.shade200,
-                borderRadius: BorderRadius.circular(4),
-              ),
-              width: double.infinity,
-              padding: const EdgeInsets.all(10),
-              // this should be a third of screen, or a bit less
-              // TODO not sure if this can be done porgrammatically with styling? - need to check to change size of container reposnisvely - maybe with expanded or flexible
-              // height: 150,
-              // width: 50,
-              // color: Colors.grey.shade200,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  const Text(
-                    "Daily Budget",
-                    style: TextStyle(
-                      fontSize: 12,
-                    ),
-                  ),
-                  Text(
-                    "DECEMBER 2021",
-                    style: TextStyle(
-                      fontSize: 12,
-                      color: Colors.grey.shade600,
-                    ),
-                  ),
-                  const SizedBox(
-                    height: 5,
-                  ),
-                  Text(
-                    // TODO if add EUR , there is overflow - fix it
-                    "${currentMonthDailyBudget.amount}",
-                    style: const TextStyle(
-                      fontSize: 24,
-                      // color: Colors.grey.shade600,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  const SizedBox(
-                    height: 1,
-                  ),
-                  GestureDetector(
-                    onTap: () {
-                      showModalBottomSheet(
-                        isScrollControlled: true,
-                        isDismissible: false,
-                        context: context,
-                        builder: (context) {
-                          return ModalBottomSheetWrapper(
-                            child: EditMonthDailyBudgetNew(
-                              // onCancelEdit: () => Navigator.of(context).pop(),
-                              // onSaveEdit: () => Navigator.of(context).pop(),
-                              onClose: () {
-                                // TODO this is a bit silly
-                                // maybe top button this could call its own stuff, so only top button is reloaded.
-                                // this way, everything is reloaded. come back to this
-                                onLoadCurrentMonthDailyBudget();
-                                Navigator.of(context).pop();
-                                // TODO
-                              },
-                              currentMonthDailyBudget: currentMonthDailyBudget,
-                            ),
-                          );
-                        },
-                      );
-
-                      // showModalBottomSheet(
-                      //   // NOTE: TOOK from here https://stackoverflow.com/a/57515977/9661910
-                      //   // and here https://stackoverflow.com/a/75572237/9661910
-                      //   // TODO use this if there is anything scrollable
-                      //   isScrollControlled: true,
-                      //   isDismissible: false,
-                      //   context: context,
-                      //   builder: (context) {
-                      //     // TODO create ModalBottomSheetWrapper in same style as DialogWrapper
-                      //     return Container(
-                      //       // height: 200,
-                      //       color: Colors.white,
-                      //       // padding: const EdgeInsets.all(15.0),
-                      //       padding: EdgeInsets.only(
-                      //         bottom: MediaQuery.of(context).viewInsets.bottom,
-                      //         top: 15,
-                      //         left: 15,
-                      //         right: 15,
-                      //       ),
-                      //       // child: const _HomeScreenAddExpense(),
-                      //       // child: const Text("Edit Budget"),
-                      //       child: const EditMonthDailyBudget(),
-                      //     );
-                      //   },
-                      // );
-                    },
-                    child: Row(
-                      children: [
-                        Icon(
-                          Icons.edit,
-                          color: Colors.grey.shade600,
-                          size: 12,
-                        ),
-                        const SizedBox(
-                          width: 5,
-                        ),
-                        Text(
-                          "Edit Budget",
-                          style: TextStyle(
-                            color: Colors.grey.shade600,
-                            fontSize: 10,
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                ],
-              ),
+            child: _CurrentMonthDailyBudget(
+              currentMonthDailyBudget: currentMonthDailyBudget,
+              onLoadCurrentMonthDailyBudget: onLoadCurrentMonthDailyBudget,
             ),
           ),
           const SizedBox(
             width: 15,
           ),
-          Flexible(
-            child: GestureDetector(
-              onTap: () {
-                // final nowDate = DateTime.now();
-
-                // DateTime.april;
-
-                // final january = DateTime(2024, 1, 1);
-                // // final firstMillisecondOfJanuary = DateTime(
-                // //     january.year, january.month, january.day, 0, 0, 0, 0);
-
-                // print("nowDate: $nowDate");
-
-                Navigator.of(context).push(
-                  MaterialPageRoute(
-                    builder: (context) {
-                      return const BalanceReportScreen();
-                    },
-                  ),
-                );
-              },
-              child: Container(
-                width: double.infinity,
-                padding: const EdgeInsets.all(10),
-                // height: 150,
-                // width: 100,
-                decoration: BoxDecoration(
-                  color: Colors.grey.shade200,
-                  borderRadius: BorderRadius.circular(4),
-                ),
-                // color: Colors.grey.shade200,
-                child: const Column(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Icon(
-                      Icons.analytics,
-                      size: 64,
-                    ),
-                    Text(
-                      "Balance Report",
-                      textAlign: TextAlign.center,
-                      style: TextStyle(
-                        fontSize: 14,
-                        // color: Colors.white,
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            ),
+          const Flexible(
+            child: _BalanceReport(),
           ),
           const SizedBox(
             width: 15,
           ),
-          Flexible(
-            child: GestureDetector(
-              onTap: () {
-                showModalBottomSheet(
-                  // NOTE: TOOK from here https://stackoverflow.com/a/57515977/9661910
-                  // and here https://stackoverflow.com/a/75572237/9661910
-                  isScrollControlled: true,
-                  isDismissible: false,
-                  context: context,
-                  builder: (context) {
-                    return Container(
-                      // height: 200,
-                      color: Colors.white,
-                      // padding: const EdgeInsets.all(15.0),
-                      padding: EdgeInsets.only(
-                        bottom: MediaQuery.of(context).viewInsets.bottom,
-                        top: 15,
-                        left: 15,
-                        right: 15,
-                      ),
-                      child: AddExpense(
-                        onClose: () => Navigator.of(context).pop(),
-                      ),
-                    );
-                  },
-                );
-              },
-              child: Container(
-                width: double.infinity,
-                padding: const EdgeInsets.all(10),
-                // height: 150,
-                // width: 100,
-                decoration: BoxDecoration(
-                  color: Colors.grey,
-                  borderRadius: BorderRadius.circular(4),
-                ),
-                // color: Colors.grey,
-                child: const Column(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Icon(
-                      Icons.add_box_rounded,
-                      color: Colors.white,
-                      size: 64,
-                    ),
-                    Text(
-                      "Add Expense",
-                      textAlign: TextAlign.center,
-                      style: TextStyle(
-                        fontSize: 14,
-                        color: Colors.white,
-                      ),
-                    ),
-                  ],
-                ),
+          const Flexible(
+            child: _AddExpense(),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class _AddExpense extends StatelessWidget {
+  const _AddExpense();
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        showModalBottomSheet(
+          // NOTE: TOOK from here https://stackoverflow.com/a/57515977/9661910
+          // and here https://stackoverflow.com/a/75572237/9661910
+          isScrollControlled: true,
+          isDismissible: false,
+          context: context,
+          builder: (context) {
+            return Container(
+              // height: 200,
+              color: Colors.white,
+              // padding: const EdgeInsets.all(15.0),
+              padding: EdgeInsets.only(
+                bottom: MediaQuery.of(context).viewInsets.bottom,
+                top: 15,
+                left: 15,
+                right: 15,
               ),
+              child: AddExpense(
+                onClose: () => Navigator.of(context).pop(),
+              ),
+            );
+          },
+        );
+      },
+      child: Container(
+        width: double.infinity,
+        padding: const EdgeInsets.all(10),
+        // height: 150,
+        // width: 100,
+        decoration: BoxDecoration(
+          color: Colors.grey,
+          borderRadius: BorderRadius.circular(4),
+        ),
+        // color: Colors.grey,
+        child: const Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Icon(
+              Icons.add_box_rounded,
+              color: Colors.white,
+              size: 64,
+            ),
+            Text(
+              "Add Expense",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 14,
+                color: Colors.white,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _BalanceReport extends StatelessWidget {
+  const _BalanceReport({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        // final nowDate = DateTime.now();
+
+        // DateTime.april;
+
+        // final january = DateTime(2024, 1, 1);
+        // // final firstMillisecondOfJanuary = DateTime(
+        // //     january.year, january.month, january.day, 0, 0, 0, 0);
+
+        // print("nowDate: $nowDate");
+
+        Navigator.of(context).push(
+          MaterialPageRoute(
+            builder: (context) {
+              return const BalanceReportScreen();
+            },
+          ),
+        );
+      },
+      child: Container(
+        width: double.infinity,
+        padding: const EdgeInsets.all(10),
+        // height: 150,
+        // width: 100,
+        decoration: BoxDecoration(
+          color: Colors.grey.shade200,
+          borderRadius: BorderRadius.circular(4),
+        ),
+        // color: Colors.grey.shade200,
+        child: const Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Icon(
+              Icons.analytics,
+              size: 64,
+            ),
+            Text(
+              "Balance Report",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 14,
+                // color: Colors.white,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _CurrentMonthDailyBudget extends StatelessWidget {
+  const _CurrentMonthDailyBudget({
+    required this.currentMonthDailyBudget,
+    required this.onLoadCurrentMonthDailyBudget,
+  });
+
+  final PeriodDailyBudgetModel currentMonthDailyBudget;
+  final VoidCallback onLoadCurrentMonthDailyBudget;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.grey.shade200,
+        borderRadius: BorderRadius.circular(4),
+      ),
+      width: double.infinity,
+      padding: const EdgeInsets.all(10),
+      // this should be a third of screen, or a bit less
+      // TODO not sure if this can be done porgrammatically with styling? - need to check to change size of container reposnisvely - maybe with expanded or flexible
+      // height: 150,
+      // width: 50,
+      // color: Colors.grey.shade200,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const Text(
+            "Daily Budget",
+            style: TextStyle(
+              fontSize: 12,
+            ),
+          ),
+          Text(
+            "DECEMBER 2021",
+            style: TextStyle(
+              fontSize: 12,
+              color: Colors.grey.shade600,
+            ),
+          ),
+          const SizedBox(
+            height: 5,
+          ),
+          Text(
+            // TODO if add EUR , there is overflow - fix it
+            "${currentMonthDailyBudget.amount}",
+            style: const TextStyle(
+              fontSize: 24,
+              // color: Colors.grey.shade600,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          const SizedBox(
+            height: 1,
+          ),
+          GestureDetector(
+            onTap: () {
+              showModalBottomSheet(
+                isScrollControlled: true,
+                isDismissible: false,
+                context: context,
+                builder: (context) {
+                  return ModalBottomSheetWrapper(
+                    child: EditMonthDailyBudgetNew(
+                      // onCancelEdit: () => Navigator.of(context).pop(),
+                      // onSaveEdit: () => Navigator.of(context).pop(),
+                      onClose: () {
+                        // TODO this is a bit silly
+                        // maybe top button this could call its own stuff, so only top button is reloaded.
+                        // this way, everything is reloaded. come back to this
+                        onLoadCurrentMonthDailyBudget();
+                        Navigator.of(context).pop();
+                        // TODO
+                      },
+                      currentMonthDailyBudget: currentMonthDailyBudget,
+                    ),
+                  );
+                },
+              );
+
+              // showModalBottomSheet(
+              //   // NOTE: TOOK from here https://stackoverflow.com/a/57515977/9661910
+              //   // and here https://stackoverflow.com/a/75572237/9661910
+              //   // TODO use this if there is anything scrollable
+              //   isScrollControlled: true,
+              //   isDismissible: false,
+              //   context: context,
+              //   builder: (context) {
+              //     // TODO create ModalBottomSheetWrapper in same style as DialogWrapper
+              //     return Container(
+              //       // height: 200,
+              //       color: Colors.white,
+              //       // padding: const EdgeInsets.all(15.0),
+              //       padding: EdgeInsets.only(
+              //         bottom: MediaQuery.of(context).viewInsets.bottom,
+              //         top: 15,
+              //         left: 15,
+              //         right: 15,
+              //       ),
+              //       // child: const _HomeScreenAddExpense(),
+              //       // child: const Text("Edit Budget"),
+              //       child: const EditMonthDailyBudget(),
+              //     );
+              //   },
+              // );
+            },
+            child: Row(
+              children: [
+                Icon(
+                  Icons.edit,
+                  color: Colors.grey.shade600,
+                  size: 12,
+                ),
+                const SizedBox(
+                  width: 5,
+                ),
+                Text(
+                  "Edit Budget",
+                  style: TextStyle(
+                    color: Colors.grey.shade600,
+                    fontSize: 10,
+                  ),
+                ),
+              ],
             ),
           ),
         ],
