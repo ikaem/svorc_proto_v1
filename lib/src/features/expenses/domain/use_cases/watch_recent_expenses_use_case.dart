f2387ece99cc0ef6820b26a7c8baa210716e28f4
Karlo M <karlo.marinovic@decode.agency>
2024-12-16 20:06:52 +0100

    test use case

---
+++ lib/src/features/expenses/domain/use_cases/watch_recent_expenses_use_case.dart
@@ -0,0 +1,23 @@
+import 'package:svorc_proto_v1/src/features/expenses/data/data_sources/local/expenses_local_data_source.dart';
+import 'package:svorc_proto_v1/src/features/expenses/domain/models/expense_model.dart';
+import 'package:svorc_proto_v1/src/features/expenses/domain/repositories/expenses_repository.dart';
+
+class WatchRecentExpensesUseCase {
+  const WatchRecentExpensesUseCase({
+    required ExpensesRepository expensesRepository,
+  }) : _expensesRepository = expensesRepository;
+
+  final ExpensesRepository _expensesRepository;
+
+  Stream<List<ExpenseModel>> call() {
+    const GetExpensesFilterValue filter = GetExpensesFilterValue(
+      limit: 5,
+    );
+
+    final Stream<List<ExpenseModel>> stream = _expensesRepository.watchExpenses(
+      filter: filter,
+    );
+
+    return stream;
+  }
+}
---
+++ test/unit/src/features/expenses/domain/use_cases/watch_recent_expenses_use_case_test.dart
@@ -0,0 +1,87 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:svorc_proto_v1/src/features/categories/domain/models/category_model.dart';
+import 'package:svorc_proto_v1/src/features/expenses/data/data_sources/local/expenses_local_data_source.dart';
+import 'package:svorc_proto_v1/src/features/expenses/domain/models/expense_model.dart';
+import 'package:svorc_proto_v1/src/features/expenses/domain/repositories/expenses_repository.dart';
+import 'package:svorc_proto_v1/src/features/expenses/domain/use_cases/watch_recent_expenses_use_case.dart';
+
+void main() {
+  final ExpensesRepository expensesRepository = _MockExpensesRepository();
+
+// tested class
+  final WatchRecentExpensesUseCase useCase =
+      WatchRecentExpensesUseCase(expensesRepository: expensesRepository);
+
+  setUpAll(() {
+    registerFallbackValue(_FakeGetExpensesFilterValue());
+  });
+
+  tearDown(() {
+    reset(expensesRepository);
+  });
+
+  group(
+    WatchRecentExpensesUseCase,
+    () {
+      group(
+        ".call",
+        () {
+          test(
+            "given nothing in particular"
+            "when [call] is called "
+            "then should call [ExpensesRepository.watchExpenses] with correct arguments and return expected result",
+            () async {
+              // setup
+              final List<ExpenseModel> expensesModels = List.generate(
+                5,
+                (index) => ExpenseModel(
+                  id: index + 1,
+                  date: DateTime.now().add(Duration(days: index)),
+                  amount: 100 * (index + 1),
+                  category: CategoryModel(
+                    id: index + 1,
+                    name: "Category $index",
+                  ),
+                ),
+              );
+
+              final Stream<List<ExpenseModel>> stream =
+                  Stream.value(expensesModels);
+
+              when(
+                () => expensesRepository.watchExpenses(
+                    filter: any(named: "filter")),
+              ).thenAnswer(
+                (_) => stream,
+              );
+
+              // given
+
+              // when
+              final Stream<List<ExpenseModel>> result = useCase();
+
+              // then
+
+              verify(
+                () => expensesRepository.watchExpenses(
+                  filter: const GetExpensesFilterValue(
+                    limit: 5,
+                  ),
+                ),
+              ).called(1);
+              expect(result, emits(expensesModels));
+
+              // cleanup
+            },
+          );
+        },
+      );
+    },
+  );
+}
+
+class _MockExpensesRepository extends Mock implements ExpensesRepository {}
+
+class _FakeGetExpensesFilterValue extends Fake
+    implements GetExpensesFilterValue {}
